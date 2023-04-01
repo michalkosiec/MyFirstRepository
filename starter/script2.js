4e03a5197a0c1fbf7e2f726922a211e92e138410
michalkosiec <109213634+michalkosiec@users.noreply.github.com>
2023-04-01 11:25:49 +0200

    Add files via upload

---
+++ starter/script2.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const obj1 = {
+  arr1: [1, 2, 3, 4],
+  method1() {
+    return this.arr1.filter(el => {
+      console.log(this);
+      return el % 2 === 0;
+    });
+  },
+  method2(callback) {
+    return this.arr1.filter(callback);
+  },
+};
+
+const arrow = el => {
+  console.log(this);
+  return el % 2 === 0;
+};
+
+obj1.method1();
+obj1.method2(arrow);
+obj1.method2(el => {
+  console.log(this);
+  return el % 2 === 0;
+});
